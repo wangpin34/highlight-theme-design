9f902b20e3e1efbd97bfa7192147dc6bada1c254
penn-wang <guyusay@gmail.com>
2023-10-17 16:27:10 +0800

    feat: update tab icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { RecoilRoot } from 'recoil'
 import './App.css'
-import { Theme, Box } from '@radix-ui/themes';
+import { Theme, Box } from '@radix-ui/themes'
 import Root from 'components/root'
 
 function App() {
   return (
     <RecoilRoot>
-      <Theme>
+      <Theme accentColor="pink">
         <Box className="w-screen h-screen max-h-screen">
           <Root />
         </Box>
